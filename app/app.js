e8e4b5d63ff749d5b96c7eb64200f1a916994632
rightlag <rightlag@gmail.com>
2015-02-22 03:02:26 +0000

    Initial commit

--- app/app.js
+++ app/app.js
@@ -3,10 +3,21 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
+  'ngResource',
+  'myApp.servo',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
+])
+.constant('BASE_URL', 'http://confident-tornado-80-198004.use1-2.nitrousbox.com')
+.factory('Service', ['BASE_URL', '$resource', function(BASE_URL, $resource) {
+  return {
+    station: $resource(
+      BASE_URL + '/stations/:id',
+      { id: '@id' }
+    )
+  }
+}])
+.config(['$routeProvider', '$resourceProvider',
+         function($routeProvider, $resourceProvider) {
   $routeProvider.otherwise({redirectTo: '/view1'});
+  $resourceProvider.defaults.stripTrailingSlashes = false;
 }]);
